7a6085861cb6fd564aef61d99ff1fbe8354c1ace
Clydewtf <legitskull@mail.ru>
2025-10-03 16:55:34 +0700

    chore(migrations): configure alembic env & remove create_all from main

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,7 @@ from app.core.db import engine, Base
 from app.models import user, transaction
 
 app = FastAPI(title=settings.PROJECT_NAME)
-Base.metadata.create_all(bind=engine)
+#Base.metadata.create_all(bind=engine)
 
 @app.get("/ping")
 def health_check():
