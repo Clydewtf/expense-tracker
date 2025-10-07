9e46521d7c87f06262b40757fd4caf2355c3bf39
Clydewtf <legitskull@mail.ru>
2025-10-07 18:16:09 +0700

    feat(api): add route /rates

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,10 +4,12 @@ from app.core.db import engine, Base
 from app.models import user, transaction
 from app.api.users import router as user_router
 from app.api.transactions import router as transaction_router
+from app.api.rates import router as rate_router
 
 app = FastAPI(title=settings.PROJECT_NAME)
 app.include_router(user_router)
 app.include_router(transaction_router)
+app.include_router(rate_router)
 #Base.metadata.create_all(bind=engine)
 
 @app.get("/ping")
